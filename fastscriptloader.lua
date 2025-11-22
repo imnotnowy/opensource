55a681e22b18daccd0c27c3c69f771f9910e9638
Nowy:3 <egesdabest@gmail.com>
2025-11-22 19:04:09 +0300

    Create fastscriptloader.lua

---
+++ fastscriptloader.lua
@@ -0,0 +1,58 @@
+wait(2.5)
+--[[local script = [[
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/imnotnowy/obsireworked/main/Library.lua"))()
+local win = lib:CreateWindow({Title="Lumina.wtf",Icon="rbxassetid://99312207847076",Footer="Lumina | Fast Script Loader | v1.1",NotifySide="Right",Size=UDim2.fromOffset(600, 666),Resizable=true,Center=true})
+local mainT = win:AddTab("Main","list")
+local left = mainT:AddLeftGroupbox("Scripts","house")
+local function cum() lib:Unload() end
+left:AddButton({Text="Forsaken",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidi399/Veronica-stuff/refs/heads/main/Auto%20trick"))() loadstring(game:HttpGet("https://raw.githubusercontent.com/gloveringdevting/GlovScripts-Repository/refs/heads/main/GlovSaken%20(Main)%20Loader"))() cum() end})
+left:AddLabel("Glovsaken | Auto Veeronica Trick (Mobile only)", true)
+left:AddDivider()
+left:AddButton({Text="Lumina",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/imnotnowy/Lumina/refs/heads/main/Loader.luau"))() cum() end})
+left:AddLabel("Die of Death | Universal", true)
+left:AddDivider()
+left:AddButton({Text="IY",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))() cum() end})
+left:AddButton({Text="Dex",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua")) cum() end})
+left:AddButton({Text="Rspy",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/SimpleSpyV3/main.lua"))() cum() end})
+left:AddButton({Text="Hspy",Func=function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Best-HTTP-SPY-38448"))() cum() end})
+left:AddDivider()
+left:AddButton({Text="SynX",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-s-scripts/main/Synapse-X-Remake.lua"))() cum() end})
+left:AddDivider()
+local right = mainT:AddRightGroupbox("Other","ellipsis")
+right:AddButton({Text="Discord",Func=function() setclipboard("https://discord.gg/xnUgd8yQG6") end}):AddButton({Text="Unload",Func=function() cum() end})
+right:AddLabel("credits to the owners of the scripts", true)
+]]
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/imnotnowy/obsireworked/main/Library.lua"))()
+local win = lib:CreateWindow({Title="Lumina.wtf",Icon="rbxassetid://99312207847076",Footer="Lumina | Fast Script Loader | v1.1",NotifySide="Right",Size=UDim2.fromOffset(600, 666),Resizable=true,Center=true})
+local mainT = win:AddTab("Main","list")
+local left = mainT:AddLeftGroupbox("Scripts","house")
+local function cum() lib:Unload() end
+left:AddButton({Text="Forsaken",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidi399/Veronica-stuff/refs/heads/main/Auto%20trick"))() loadstring(game:HttpGet("https://raw.githubusercontent.com/gloveringdevting/GlovScripts-Repository/refs/heads/main/GlovSaken%20(Main)%20Loader"))() cum() end})
+left:AddLabel("Glovsaken | Auto Veeronica Trick (Mobile only)", true)
+left:AddDivider()
+left:AddButton({Text="Lumina",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/imnotnowy/Lumina/refs/heads/main/Loader.luau"))() cum() end})
+left:AddLabel("Die of Death | Universal", true)
+left:AddDivider()
+left:AddButton({Text="IY",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))() cum() end})
+left:AddButton({Text="Dex",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua")) cum() end})
+left:AddButton({Text="Rspy",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/SimpleSpyV3/main.lua"))() cum() end})
+left:AddButton({Text="Hspy",Func=function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Best-HTTP-SPY-38448"))() cum() end})
+left:AddDivider()
+left:AddButton({Text="SynX",Func=function() loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-s-scripts/main/Synapse-X-Remake.lua"))() cum() end})
+left:AddDivider()
+
+local right = mainT:AddRightGroupbox("Other","ellipsis")
+right:AddButton({Text="Discord",Func=function() setclipboard("https://discord.gg/xnUgd8yQG6") end}):AddButton({Text="Unload",Func=function() cum() end})
+right:AddLabel("credits to the owners of the scripts", true)
+print([[
+dev notes:
+🥷 tried sneaking his own script LMFAOOO —f
+fun fact: i have the infinite N word pass —n
+also this is how our scripts look —n
+#fuckindentation —n
+#meandmyboyshateindentation —f
+HelloWorld("print") —f
+this mf put a loader inside a loader lmao —f
+what is this diddyblud doing —f
+added wait because forsaken —n
+]])
