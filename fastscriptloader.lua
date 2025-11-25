9dd15cb6bbc2cf95be5b4d21fca5d089f5ab06b6
Nowy:3 <egesdabest@gmail.com>
2025-11-25 22:10:38 +0300

    Update fastscriptloader.lua

--- fastscriptloader.lua
+++ fastscriptloader.lua
@@ -1,3 +1,50 @@
--- i hope you get annoyed by the way i coded this.
+--// i hope you get annoyed by the way i coded this \\--
 if game.PlaceId == 18687417158 then loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidi399/Veronica-stuff/refs/heads/main/Auto%20trick"))() loadstring(game:HttpGet("https://raw.githubusercontent.com/gloveringdevting/GlovScripts-Repository/refs/heads/main/GlovSaken%20(Main)%20Loader"))() end
+--^^^forsaken^^^--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if game.PlaceId == 6005930507 then loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))() end
