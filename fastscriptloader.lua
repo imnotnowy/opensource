a75173321029b66136ed89f1541211a610a2e1ec
Nowy:3 <egesdabest@gmail.com>
2025-11-27 03:18:45 +0300

    Update fastscriptloader.lua

--- fastscriptloader.lua
+++ fastscriptloader.lua
@@ -1,9 +1,16 @@
+local etohIds = {
+  9070958509, -- ring 0
+  9070657865, -- ring 1
+  9070954594, -- forgotten ridge
+  9070979698, -- ring 2
+  9071001075, -- zone 1
+  9071001563 -- zone 3
+}
 --// i hope you get annoyed by the way i coded this \\--
 if game.PlaceId == 18687417158 then loadstring(game:HttpGet("https://raw.githubusercontent.com/skibidi399/Veronica-stuff/refs/heads/main/Auto%20trick"))() loadstring(game:HttpGet("https://raw.githubusercontent.com/gloveringdevting/GlovScripts-Repository/refs/heads/main/GlovSaken%20(Main)%20Loader"))() end
 --^^^forsaken^^^--
-
-
-
+if game.PlaceId == etohIds then loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() end
+--^^^etoh iy^^--
 
 
 
